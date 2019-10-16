d7d69b8aef2ae1a54261b0fd4e2f05e058f1bb58
hyper-88 <alexk666@yandex.ru>
2019-10-16 21:34:51 +0500

    Релиз 4.4 правленный

--- js/films.js
+++ js/films.js
@@ -1,41 +1,52 @@
-let genres = ['фантастика', 'боевик', 'приключения',
-'фэнтези', 'драмма', 'комедия', 'мультфильм']
-
+let genres = ['фантастика', 'боевик', 'приключения', 'фэнтези', 'драмма', 'комедия', 'мультфильм']
 let films = [
     ['10:00', 'Человек-паук', 0],
     ['12:00', 'Собачья жизнь 2', 3],
     ['14:00', 'История игрушек 4', 6],
     ['16:00', 'Люди в черном: Интернэшнл', 0]
 ]
-console.log(films[0][2])
+let film = new Array
+for (let i = 0; i < films.length; i++) {
+    for (let y = 0; y < films[i].length; y++) {
+        film[y] = films[i][y];
+        console.log(film.length);
+        switch (i) {
+            case 0:
+                let element_start_film_1 = document.getElementById('start_film_1');
+                let element_name_film_1 = document.getElementById('name_film_1');
+                let element_genre_film_1 = document.getElementById('genre_film_1');
+                element_start_film_1.innerHTML = film[0];
+                element_name_film_1.innerHTML = film[1];
+                element_genre_film_1.innerHTML = genres[film[2]];
+                break;
+            case 1:
+                let element_start_film_2 = document.getElementById('start_film_2');
+                let element_name_film_2 = document.getElementById('name_film_2');
+                let element_genre_film_2 = document.getElementById('genre_film_2');
+                element_start_film_2.innerHTML = film[0];
+                element_name_film_2.innerHTML = film[1];
+                element_genre_film_2.innerHTML = genres[film[2]];
+                break;
+            case 2:
+                let element_start_film_3 = document.getElementById('start_film_3');
+                let element_name_film_3 = document.getElementById('name_film_3');
+                let element_genre_film_3 = document.getElementById('genre_film_3');
+                element_start_film_3.innerHTML = film[0];
+                element_name_film_3.innerHTML = film[1];
+                element_genre_film_3.innerHTML = genres[film[2]];
+                break;
+            case 3:
+                let element_start_film_4 = document.getElementById('start_film_4');
+                let element_name_film_4 = document.getElementById('name_film_4');
+                let element_genre_film_4 = document.getElementById('genre_film_4');
+                element_start_film_4.innerHTML = film[0];
+                element_name_film_4.innerHTML = film[1];
+                element_genre_film_4.innerHTML = genres[film[2]];
+                break;
+        }
+    }
+}
 
-let element_start_film_1 = document.getElementById('start_film_1')
-let element_name_film_1 = document.getElementById('name_film_1')
-let element_genre_film_1 = document.getElementById('genre_film_1')
-element_start_film_1.innerHTML = films[0][0]
-element_name_film_1.innerHTML = films[0][1]
-element_genre_film_1.innerHTML = genres[films[0][2]]
-
-let element_start_film_2 = document.getElementById('start_film_2')
-let element_name_film_2 = document.getElementById('name_film_2')
-let element_genre_film_2 = document.getElementById('genre_film_2')
-element_start_film_2.innerHTML = films[1][0]
-element_name_film_2.innerHTML = films[1][1]
-element_genre_film_2.innerHTML = genres[films[1][2]]
-
-let element_start_film_3 = document.getElementById('start_film_3')
-let element_name_film_3 = document.getElementById('name_film_3')
-let element_genre_film_3 = document.getElementById('genre_film_3')
-element_start_film_3.innerHTML = films[2][0]
-element_name_film_3.innerHTML = films[2][1]
-element_genre_film_3.innerHTML = genres[films[2][2]]
-
-let element_start_film_4 = document.getElementById('start_film_4')
-let element_name_film_4 = document.getElementById('name_film_4')
-let element_genre_film_4 = document.getElementById('genre_film_4')
-element_start_film_4.innerHTML = films[3][0]
-element_name_film_4.innerHTML = films[3][1]
-element_genre_film_4.innerHTML = genres[films[3][2]]
 
 // console.log('start_film_1')
 // const genre_2 = 'боевик'
